95357d157e56285b5764bdcc64fa8dc86a248aa6
fajarmelfiano <fajarmelfiano@gmail.com>
2026-05-05 21:20:00 +0700

    moga

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'your_super_secret_jwt_key',
-  adminUsername: process.env.ADMIN_USERNAME || 'admin',
-  adminPassword: process.env.ADMIN_PASSWORD || 'admin123',
-  jwtExpire: '24h'
-};
