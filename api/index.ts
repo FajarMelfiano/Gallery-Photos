571d61fe1c9ae3454a375ca6c068c4a88f88cb90
fajarmelfiano <101026953+FajarMelfiano@users.noreply.github.com>
2026-05-06 09:48:08 +0700

    Add files via upload

--- api/index.ts
+++ api/index.ts
@@ -34,6 +34,17 @@ app.get("/api/photos/view/:driveId", async (req, res) => {
     }
     res.set("Cache-Control", "public, max-age=31536000");
 
+    if (req.query.download) {
+      let filename = "photo.jpg";
+      if (typeof req.query.filename === "string" && req.query.filename.trim() !== "") {
+        filename = req.query.filename.replace(/[^a-zA-Z0-9_\-\. ]/g, "");
+        if (!filename.toLowerCase().endsWith(".jpg") && !filename.toLowerCase().endsWith(".png")) {
+          filename += ".jpg";
+        }
+      }
+      res.set("Content-Disposition", `attachment; filename="${filename}"`);
+    }
+
     response.data.pipe(res);
   } catch (error: any) {
     console.error("Image Proxy Error:", error.message);
